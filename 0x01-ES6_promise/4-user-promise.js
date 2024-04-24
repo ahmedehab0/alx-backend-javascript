f2863dc23cc65ebf8e45b2f310bb646a0e8d1cec
ahmedehab0 <ahmed.eh01@gmail.com>
2024-04-24 12:06:26 -0400

    script

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,7 +1,6 @@
 export default function signUpUser(firstName, lastName){
-	return new Promise((res, rej) => {
-		res({
-			firstName,
-			lastName})
-	});
+	return new Promise.res({	
+		firstName,
+		lastName})
+	);
 }
